7c4290e650828418464da150e4c4d5e1bbbc9f9a
Florent Guillaume <fguillaume@apache.org>
2009-05-28 11:31:03 +0000

    CMIS-18: remove Nuxeo copyright
    git-svn-id: https://svn.apache.org/repos/asf/incubator/chemistry/trunk@779568 13f79535-47bb-0310-9956-ffa450edef68

--- chemistry/chemistry-atompub-server/src/main/java/org/apache/chemistry/atompub/server/CMISServiceResponse.java
+++ chemistry/chemistry-atompub-server/src/main/java/org/apache/chemistry/atompub/server/CMISServiceResponse.java
@@ -1,6 +1,4 @@
 /*
- * Copyright 2009 Nuxeo SA <http://nuxeo.com>
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,7 +12,7 @@
  * limitations under the License.
  *
  * Authors:
- *     Florent Guillaume
+ *     Florent Guillaume, Nuxeo
  */
 package org.apache.chemistry.atompub.server;
 
